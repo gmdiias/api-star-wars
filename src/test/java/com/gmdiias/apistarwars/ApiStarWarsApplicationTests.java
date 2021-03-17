b28414319cedf48983e7dad09c6185192804becd
Gustavo Mendonça <gustavo.dias@iadtec.com.br>
2021-03-17 01:43:27 -0300

    add dependencia do h2 para execucao de testes automatizados

--- src/test/java/com/gmdiias/apistarwars/ApiStarWarsApplicationTests.java
+++ src/test/java/com/gmdiias/apistarwars/ApiStarWarsApplicationTests.java
@@ -1,13 +1,10 @@
 package com.gmdiias.apistarwars;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-class ApiStarWarsApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
+@ActiveProfiles("test")
+public class ApiStarWarsApplicationTests {
 
 }
